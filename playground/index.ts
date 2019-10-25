bfbef382744dd9bdb5fa30aa0fc6ed777fb2ed17
Jaymun723 <jaymun723@yahoo.com>
2019-10-25 12:31:17 +0200

    Bump version to 1.0.2, enchance playground.

--- playground/index.ts
+++ playground/index.ts
@@ -7,10 +7,20 @@ document.body.appendChild(plot.canvas)
 
 const genPoints = (nums: number[]) => nums.map((num, i) => ({ x: i, y: num }))
 const points = genPoints([5, 6, 7, 2, 9, 4, 3])
+const endArr = (i: number, arr: any[]) => arr[arr.length + i]
 
 // @ts-ignore
 window.plot = plot
 
-plot.drawSegment(p(0, 0), p(5, 5))
-plot.drawPoints(points)
-plot.drawLine(2, 1)
+plot.onClick = (p) => {
+  points.push(p)
+  draw()
+}
+
+const draw = () => {
+  plot.clear()
+  plot.drawSegment(endArr(-1, points), endArr(-2, points))
+  plot.drawPoints(points)
+  plot.drawLine(2, 1)
+}
+draw()
